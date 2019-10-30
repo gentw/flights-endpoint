5e8dc2fd71e107d69190d3e21dd2c9755dd6dba4
gentw <gentiannuka@gmail.com>
2019-10-30 15:39:13 +0100

    creating models, relationship between models

--- app/Airport.php
+++ app/Airport.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Flight;
+
 
 class Airport extends Model
 {
-    //
+    public function arrivingFlights() {
+    	return $this->hasMany('Flight', 'arrivalAirport_id')
+    }
+
+    public function departingFlights() {
+    	return $this->hasMany('Flight', 'departureAirport_id');
+    }
 }
--- app/Flight.php
+++ app/Flight.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Airport;
+use App\Customer;
 
 class Flight extends Model
 {
-    //
+    public function arrivalAirport() {
+    	return $this->belongsTo('Airport', 'arrivalAirport_id');
+    }
+
+    public function departureAirport() {
+    	return $this->belongsTo('Airport', 'departureAirport_id');
+    }
+
+    public function passengers() {
+    	return $this->belongsToMany('Customer', 'flight_customer');
+    }
 }
