5bfc00568c7947428d74f467961eb21a62148a2f
gentw <gentiannuka@gmail.com>
2019-11-03 00:17:22 +0100

    flights can be updated and deleted throught api (handling put and delete requests)

--- app/Http/Controllers/v1/FlightController.php
+++ app/Http/Controllers/v1/FlightController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Services\v1\FlightService;
 use Illuminate\Http\Request;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 
 
 class FlightController extends Controller
@@ -92,7 +94,16 @@ class FlightController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            $flight = $this->flights->updateFlight($request, $id);
+            return response()->json($flight, 200);
+        } 
+        catch (ModelNotFoundException $ex) {
+            throw $ex;
+        }
+        catch (Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -103,6 +114,15 @@ class FlightController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try {
+            $flight = $this->flights->deleteFlight($id);
+            return response()->make('', 204);
+        } 
+        catch (ModelNotFoundException $ex) {
+            throw $ex;
+        }
+        catch (Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
     }
 }
--- app/Services/v1/FlightService.php
+++ app/Services/v1/FlightService.php
@@ -55,6 +55,32 @@ class FlightService {
         return $this->filterFlights([$flight]);
 	}
 
+	public function updateFlight($req, $flightNumber) {
+		$flight = Flight::where('flightNumber', $flightNumber)->firstOrFail();
+		$arrivalAirport = $req->input('arrival.iataCode');
+        $departureAirport = $req->input('departure.iataCode');
+        $airports = Airport::whereIn('iataCode', [$arrivalAirport, $departureAirport])->get();
+        $codes = [];
+
+        foreach($airports as $port) {
+        	$codes[$port->iataCode] = $port->id;
+        }
+
+        $flight->flightNumber = $req->input('flightNumber');
+        $flight->status = $req->input('status');
+        $flight->arrivalAirport_id = $codes[$arrivalAirport];
+        $flight->arrivalDateTime = $req->input('arrival.datetime');
+        $flight->departureAirport_id = $codes[$departureAirport];
+        $flight->departureDateTime = $req->input('departure.datetime');
+        $flight->save();
+        return $this->filterFlights([$flight]);
+	}
+
+	public function deleteFlight($flightNumber) {
+        $flight = Flight::where('flightNumber', $flightNumber)->firstOrFail();
+        
+        $flight->delete();
+    }
 
 	// public function getFlight($flightNumber) {
 	// 	return $this->filterFlights(Flight::where('flightNumber', $flightNumber)->get());
